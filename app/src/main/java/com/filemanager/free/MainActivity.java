918f27ad28efb90021449e020094b38ce1ab3d33
Hoang Hiep <hoanghiep.hui@gmail.com>
2016-03-06 12:59:29 +0700

    Bắt đầu tạo dự án và import các thư viện

---
+++ app/src/main/java/com/filemanager/free/MainActivity.java
@@ -0,0 +1,9 @@
+package com.filemanager.free;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Hoang Hiep on 06/03/2016.
+ */
+public class MainActivity extends AppCompatActivity {
+}
